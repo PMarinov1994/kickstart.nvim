61a13a92ff0bedd35777ac00083b0fef201da854
PMarinov1994 <plamen_944@outlook.com>
2024-08-30 13:04:10 +0300

    Remove print msg from lua/config/keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-print 'TTTTTTTTTTTTTTTT'
 -- Disable line detele at cursor
 vim.api.nvim_set_keymap('i', '<C-U>', '<nop>', {})
 --
