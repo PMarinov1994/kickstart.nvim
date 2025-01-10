439736da01764679d23e2ea31f98304e3d3f677f
PMarinov1994 <plamen_944@outlook.com>
2025-01-10 17:41:49 +0200

    Change trouble (diagnostic view)'s key binds a bit

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -4,12 +4,17 @@ return {
   cmd = 'Trouble',
   keys = {
     {
-      '<leader>xx',
+      '<leader>xa',
       '<cmd>Trouble diagnostics toggle<cr>',
       desc = 'Diagnostics (Trouble)',
     },
     {
-      '<leader>xX',
+      '<leader>xx',
+      '<cmd>Trouble diagnostics toggle filter.severity=vim.diagnostic.severity.ERROR<cr>',
+      desc = 'Diagnostics Errors Only (Trouble)',
+    },
+    {
+      '<leader>xc',
       '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
       desc = 'Buffer Diagnostics (Trouble)',
     },
