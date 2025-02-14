cacb46bdaf98f3f41312cc3ea2e8bc947e8b57fd
PMarinov1994 <plamen_944@outlook.com>
2025-02-14 16:23:48 +0200

    Add Go LSP

--- init.lua
+++ init.lua
@@ -622,7 +622,7 @@ require('lazy').setup({
             '--query-driver=gcc',
           },
         },
-        -- gopls = {},
+        gopls = {},
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
