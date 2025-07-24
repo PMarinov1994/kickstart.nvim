16520564f2d1a8c9d58e6e23392cba079382a973
PMarinov1994 <plamen_944@outlook.com>
2025-07-24 23:11:13 +0300

    Add dotnet nvim-dap configuration

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -285,5 +285,23 @@ return {
     }
 
     require('dap-python').setup 'python3'
+
+    -- Install 'netcoredbg' from somewhere
+    dap.adapters.coreclr = {
+      type = 'executable',
+      command = 'netcoredbg',
+      args = { '--interpreter=vscode' },
+    }
+
+    dap.configurations.cs = {
+      {
+        type = 'coreclr',
+        name = 'launch - netcoredbg',
+        request = 'launch',
+        program = function()
+          return vim.fn.input('Path to dll: ', vim.fn.getcwd(), 'file')
+        end,
+      },
+    }
   end,
 }
